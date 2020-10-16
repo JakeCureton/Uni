e960d653064a892252f9416a42502daab6bd4800
JakeCureton <72709043+JakeCureton@users.noreply.github.com>
2020-10-16 01:43:29 +0100

    Update keypad.py

--- keypad.py
+++ keypad.py
@@ -41,3 +41,29 @@ def keypadpin(key):
             print("Invalid code")
 
 keypad.registerKeyPressHandler(keypadpin)'''
+
+
+#and this
+'''from pad4pi import rpi_gpio
+
+KEYPAD = [
+   [   '1','2','3','A',    
+            '4','5','6','B',
+            '7','8','9','C',
+            '*','0','#','D'     ]
+]
+
+ROW_PINS = [12,16,18,22] 
+COL_PINS = [19, 15, 13, 11] 
+
+factory = rpi_gpio.KeypadFactory()
+
+# Try factory.create_4_by_3_keypad
+# and factory.create_4_by_4_keypad for reasonable defaults
+keypad = factory.create_4_by_4_keypad(keypad=KEYPAD, row_pins=ROW_PINS, col_pins=COL_PINS)
+
+def printKey(key):
+    print(key)
+
+# printKey will be called each time a keypad button is pressed
+keypad.registerKeyPressHandler(printKey)'''
