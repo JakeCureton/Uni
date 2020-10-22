1faddbd9de1dcdd0f85c07f65ae787e6aca6426a
JakeCureton <72709043+JakeCureton@users.noreply.github.com>
2020-10-22 21:58:36 +0100

    last_withdraw fixed

--- customers.sql
+++ customers.sql
@@ -1,11 +1,12 @@
+CREATE DATABASE `atm`;
 CREATE TABLE `customers` (
   `firstname` varchar(50) DEFAULT NULL,
   `lastname` varchar(100) DEFAULT NULL,
   `card_id` varchar(20) DEFAULT NULL,
-  `last_withdraw` varchar(1) DEFAULT NULL
-  `card_pin` INT(20) DEFAULT NULL,
+  `last_withdraw` int(3) DEFAULT '0',
+  `card_pin` INT(4) DEFAULT NULL,
   `balance` FLOAT(200) DEFAULT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
  
-INSERT INTO `customers` VALUES ("John","Smith","4751280038571937","1","1234","500000.00");
+INSERT INTO `customers` VALUES ("John","Smith","4751280038571937","0","1234","500000.00");
 
