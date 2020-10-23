d7286a5173d6cf42936d5c20ed6fc62fa50d249f
JakeCureton <72709043+JakeCureton@users.noreply.github.com>
2020-10-23 13:44:01 +0100

    Added pin lock to db

--- customers.sql
+++ customers.sql
@@ -3,11 +3,12 @@ CREATE TABLE `customers` (
   `firstname` varchar(50) DEFAULT NULL,
   `lastname` varchar(100) DEFAULT NULL,
   `card_id` varchar(20) DEFAULT NULL,
-  `last_withdraw` int(3) DEFAULT '0',
+  `last_withdraw` int(3) DEFAULT 0 NULL,
   `card_pin` INT(4) DEFAULT NULL,
   `balance` FLOAT(200) DEFAULT NULL,
+  `pin_tries` INT(1) DEFAULT 0 NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
  
-INSERT INTO `customers` VALUES ("John","Smith","4751280038571937","0","1234","500000.00");
-INSERT INTO `customers` VALUES ("Vera","Jones","4751280038571938","0","1234","2375.28");
+INSERT INTO `customers` VALUES ("John","Smith","4751280038571937","0","1234","500000.00","0");
+INSERT INTO `customers` VALUES ("Vera","Jones","4751280038571938","0","1234","2375.28","0");
 
